fd5c7ace1160af8679b406bbf7931507fac44507
Andrea Cecchi <andrea.cecchi85@gmail.com>
2020-06-16 14:24:32 +0200

    disable incomplete types and remove unused folders

--- src/design/plone/contenttypes/interfaces/amministrazione_folder.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-from plone.app.contenttypes.interfaces import IFolder
-
-
-class IAmministrazioneFolder(IFolder):
-    """ Marker interface for AmministrazioneFolder
-    """
--- src/design/plone/contenttypes/interfaces/persone_folder.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-from plone.app.contenttypes.interfaces import IFolder
-
-
-class IPersoneFolder(IFolder):
-    """ Marker interface for PersoneFolder
-    """
