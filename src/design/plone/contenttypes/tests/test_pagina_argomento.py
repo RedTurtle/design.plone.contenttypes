80234dcb9130967b0e7e7e3e1c18ab4f907bf075
Martina Bustacchini <martina.bustacchini@redturtle.it>
2020-07-20 12:20:07 +0200

    [dev] added leadimage behavior to Pagina Argomento CT

---
+++ src/design/plone/contenttypes/tests/test_pagina_argomento.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""Setup tests for this package."""
+from design.plone.contenttypes.testing import (
+    DESIGN_PLONE_CONTENTTYPES_INTEGRATION_TESTING,
+)
+from plone import api
+import unittest
+
+
+class TestPaginaArgomento(unittest.TestCase):
+    layer = DESIGN_PLONE_CONTENTTYPES_INTEGRATION_TESTING
+
+    def setUp(self):
+        """Custom shared utility setup for tests."""
+        self.portal = self.layer["portal"]
+
+    def test_behaviors_enabled_for_pagina_argomento(self):
+        portal_types = api.portal.get_tool(name="portal_types")
+        self.assertEqual(
+            portal_types["Pagina Argomento"].behaviors,
+            (
+                "plone.namefromtitle",
+                "plone.allowdiscussion",
+                "plone.excludefromnavigation",
+                "plone.shortname",
+                "plone.ownership",
+                "plone.publication",
+                "plone.categorization",
+                "plone.basic",
+                "plone.locking",
+                "volto.blocks" "plone.leadimage",
+            ),
+        )
