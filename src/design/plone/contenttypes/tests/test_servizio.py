f545d6411e291e8692ed9ce137210bcbd351aebc
Andrea Cecchi <andrea.cecchi85@gmail.com>
2020-07-23 17:14:52 +0200

    add tests

--- src/design/plone/contenttypes/tests/test_servizio.py
+++ src/design/plone/contenttypes/tests/test_servizio.py
@@ -2,16 +2,18 @@
 """Setup tests for this package."""
 from design.plone.contenttypes.testing import (
     DESIGN_PLONE_CONTENTTYPES_API_FUNCTIONAL_TESTING,
+)  # noqa
+from design.plone.contenttypes.testing import (
     DESIGN_PLONE_CONTENTTYPES_INTEGRATION_TESTING,
-)
-from plone.app.testing import (
-    SITE_OWNER_NAME,
-    SITE_OWNER_PASSWORD,
-    TEST_USER_ID,
-    setRoles,
-)
-from plone.restapi.testing import RelativeSession
+)  # noqa
 from plone import api
+from plone.app.testing import setRoles
+from plone.app.testing import SITE_OWNER_NAME
+from plone.app.testing import SITE_OWNER_PASSWORD
+from plone.app.testing import TEST_USER_ID
+from plone.app.textfield.value import RichTextValue
+from plone.restapi.testing import RelativeSession
+
 import unittest
 
 WIDGET_PROPERTY_CHECKS = {
@@ -70,6 +72,126 @@ class TestServizio(unittest.TestCase):
         )
 
         res = api.content.find(SearchableText="sotto1")
+
+        self.assertEqual(len(res), 1)
+        self.assertEqual(res[0].UID, servizio.UID())
+
+    def test_descrizione_destinatari_indexed_in_searchabletext(self):
+
+        #  Servizio is the only ct with this field
+        servizio = api.content.create(
+            container=self.portal,
+            type="Servizio",
+            title="Test servizio",
+            descrizione_destinatari=RichTextValue(
+                raw="destinatari",
+                mimeType="text/html",
+                outputMimeType="text/html",
+                encoding="utf-8",
+            ),
+        )
+
+        res = api.content.find(SearchableText="destinatari")
+
+        self.assertEqual(len(res), 1)
+        self.assertEqual(res[0].UID, servizio.UID())
+
+    def test_chi_puo_presentare_indexed_in_searchabletext(self):
+
+        #  Servizio is the only ct with this field
+        servizio = api.content.create(
+            container=self.portal,
+            type="Servizio",
+            title="Test servizio",
+            chi_puo_presentare=RichTextValue(
+                raw="chi",
+                mimeType="text/html",
+                outputMimeType="text/html",
+                encoding="utf-8",
+            ),
+        )
+
+        res = api.content.find(SearchableText="chi")
+
+        self.assertEqual(len(res), 1)
+        self.assertEqual(res[0].UID, servizio.UID())
+
+    def test_come_si_fa_indexed_in_searchabletext(self):
+
+        #  Servizio is the only ct with this field
+        servizio = api.content.create(
+            container=self.portal,
+            type="Servizio",
+            title="Test servizio",
+            come_si_fa=RichTextValue(
+                raw="come",
+                mimeType="text/html",
+                outputMimeType="text/html",
+                encoding="utf-8",
+            ),
+        )
+
+        res = api.content.find(SearchableText="come")
+
+        self.assertEqual(len(res), 1)
+        self.assertEqual(res[0].UID, servizio.UID())
+
+    def test_cosa_si_ottiene_indexed_in_searchabletext(self):
+
+        #  Servizio is the only ct with this field
+        servizio = api.content.create(
+            container=self.portal,
+            type="Servizio",
+            title="Test servizio",
+            cosa_si_ottiene=RichTextValue(
+                raw="ottenere",
+                mimeType="text/html",
+                outputMimeType="text/html",
+                encoding="utf-8",
+            ),
+        )
+
+        res = api.content.find(SearchableText="ottenere")
+
+        self.assertEqual(len(res), 1)
+        self.assertEqual(res[0].UID, servizio.UID())
+
+    def test_cosa_serve_indexed_in_searchabletext(self):
+
+        #  Servizio is the only ct with this field
+        servizio = api.content.create(
+            container=self.portal,
+            type="Servizio",
+            title="Test servizio",
+            cosa_serve=RichTextValue(
+                raw="serve",
+                mimeType="text/html",
+                outputMimeType="text/html",
+                encoding="utf-8",
+            ),
+        )
+
+        res = api.content.find(SearchableText="serve")
+
+        self.assertEqual(len(res), 1)
+        self.assertEqual(res[0].UID, servizio.UID())
+
+    def test_box_aiuto_indexed_in_searchabletext(self):
+
+        #  Servizio is the only ct with this field
+        servizio = api.content.create(
+            container=self.portal,
+            type="Servizio",
+            title="Test servizio",
+            box_aiuto=RichTextValue(
+                raw="aiuto",
+                mimeType="text/html",
+                outputMimeType="text/html",
+                encoding="utf-8",
+            ),
+        )
+
+        res = api.content.find(SearchableText="aiuto")
 
         self.assertEqual(len(res), 1)
         self.assertEqual(res[0].UID, servizio.UID())
