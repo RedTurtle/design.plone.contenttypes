2033b65669202b8b73e3bbd64014f3275696e448
Luca Bellenghi <lucabel@gmail.com>
2020-10-28 15:24:32 +0100

    bando serializer and limit type into folder_deepening

---
+++ src/design/plone/contenttypes/restapi/serializers/bando.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from .dxcontent import SerializeFolderToJson as BaseSerializer
+from plone.restapi.interfaces import ISerializeToJson
+from zope.component import adapter
+from zope.interface import implementer
+from zope.interface import Interface
+from design.plone.contenttypes.interfaces.bando import IBandoAgidSchema
+
+
+@implementer(ISerializeToJson)
+@adapter(IBandoAgidSchema, Interface)
+class BandoSerializer(BaseSerializer):
+    def get_approfondimenti(self, bando_view):
+        """
+        """
+        folders = bando_view.retrieveFolderDeepening()
+        results = []
+
+        for folder in folders:
+            contents = bando_view.retrieveContentsOfFolderDeepening(
+                folder["path"]
+            )
+            folder.update({"children": contents})
+            results.append(folder)
+        return sorted(results, key=lambda k: k["title"])
+
+    def __call__(self, version=None, include_items=True):
+        result = super(BandoSerializer, self).__call__(
+            version=None, include_items=True
+        )
+        bando_view = self.context.restrictedTraverse("bando_view")
+        result["approfondimento"] = self.get_approfondimenti(bando_view)
+        result["bando_state"] = bando_view.getBandoState()
+        return result
