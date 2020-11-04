df0b078769eb18a847c839700cd429e381413728
Luca Bellenghi <lucabel@gmail.com>
2020-11-04 11:55:02 +0100

    improve check for scadenziario

--- src/design/plone/contenttypes/restapi/services/scadenziario/post.py
+++ src/design/plone/contenttypes/restapi/services/scadenziario/post.py
@@ -34,7 +34,9 @@ class ScadenziarioSearchPost(Service):
         if sort_order:
             sort_order = "descending" if sort_order else "ascending"
 
-        querybuilder = getMultiAdapter((self.context, self.request), name="querybuilderresults")
+        querybuilder = getMultiAdapter(
+            (self.context, self.request), name="querybuilderresults"
+        )
 
         querybuilder_parameters = dict(
             query=query,
@@ -48,7 +50,10 @@ class ScadenziarioSearchPost(Service):
 
         # Exclude "self" content item from the results when ZCatalog supports NOT UUID
         # queries and it is called on a content object.
-        if not IPloneSiteRoot.providedBy(self.context) and SUPPORT_NOT_UUID_QUERIES:
+        if (
+            not IPloneSiteRoot.providedBy(self.context)
+            and SUPPORT_NOT_UUID_QUERIES
+        ):
             querybuilder_parameters.update(
                 dict(custom_query={"UID": {"not": self.context.UID()}})
             )
@@ -56,7 +61,7 @@ class ScadenziarioSearchPost(Service):
         results = querybuilder(**querybuilder_parameters)
         brains_grouped = {}
         for brain in results:
-            if not safe_hasattr(results[0], 'start'):
+            if not safe_hasattr(brain, "start") or not brain.start:
                 continue
             brains_grouped.setdefault(
                 brain.start.strftime("%Y/%m/%d"), []
@@ -65,14 +70,10 @@ class ScadenziarioSearchPost(Service):
         keys = list(brains_grouped.keys())
         keys.sort()
 
-        return {
-            '@id': self.request.get('URL'),
-            'items': keys
-        }
+        return {"@id": self.request.get("URL"), "items": keys}
 
 
 class ScadenziarioDayPost(Service):
-
     def reply(self):
         data = json_body(self.request)
         query = data.get("query", None)
@@ -105,7 +106,10 @@ class ScadenziarioDayPost(Service):
 
         # Exclude "self" content item from the results when ZCatalog supports NOT UUID
         # queries and it is called on a content object.
-        if not IPloneSiteRoot.providedBy(self.context) and SUPPORT_NOT_UUID_QUERIES:
+        if (
+            not IPloneSiteRoot.providedBy(self.context)
+            and SUPPORT_NOT_UUID_QUERIES
+        ):
             querybuilder_parameters.update(
                 dict(custom_query={"UID": {"not": self.context.UID()}})
             )
@@ -113,7 +117,7 @@ class ScadenziarioDayPost(Service):
         results = querybuilder(**querybuilder_parameters)
         brains_grouped = {}
         for brain in results:
-            if not safe_hasattr(results[0], 'start'):
+            if not safe_hasattr(results[0], "start"):
                 continue
             brains_grouped.setdefault(
                 brain.start.strftime("%Y/%m/%d"), []
@@ -128,17 +132,17 @@ class ScadenziarioDayPost(Service):
             for brain in brains_grouped[key]:
                 results_to_be_returned[key].append(
                     {
-                        '@id': brain.getURL(),
-                        'id': brain.getId,
-                        'title': brain.Title,
-                        'text': brain.Description,
-                        'start': brain.start.strftime('%Y/%m/%d'),
-                        'type': self.context.translate(brain.portal_type),
-                        'category': brain.subject
+                        "@id": brain.getURL(),
+                        "id": brain.getId,
+                        "title": brain.Title,
+                        "text": brain.Description,
+                        "start": brain.start.strftime("%Y/%m/%d"),
+                        "type": self.context.translate(brain.portal_type),
+                        "category": brain.subject,
                     }
                 )
-                results_to_be_returned[key].sort(key=lambda x: x['title'])
+                results_to_be_returned[key].sort(key=lambda x: x["title"])
         return {
-            '@id': self.request.get('URL'),
-            'items': results_to_be_returned
+            "@id": self.request.get("URL"),
+            "items": results_to_be_returned,
         }
