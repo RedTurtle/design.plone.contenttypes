a4f725cb48060a3c7aca717afd75292f3f58646d
Martina Bustacchini <martina.bustacchini@redturtle.it>
2020-11-17 12:09:43 +0100

    [dev] added custom view for dettagli procedimenti

--- src/design/plone/contenttypes/browser/trasparenza.py
+++ src/design/plone/contenttypes/browser/trasparenza.py
@@ -11,3 +11,15 @@ class TrasparenzaView(BrowserView):
 
     def __call__(self):
         return
+
+
+class DettagliProcedimentiView(BrowserView):
+    """
+    """
+
+    def __init__(self, context, request):
+        self.context = context
+        self.request = request
+
+    def __call__(self):
+        return
