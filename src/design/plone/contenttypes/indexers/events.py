32620bcaf558649317a514e344147cbfbcc6bdc0
Luca Bellenghi <lucabel@gmail.com>
2020-12-01 08:49:32 +0100

    update vocabulary for collection

--- src/design/plone/contenttypes/indexers/events.py
+++ src/design/plone/contenttypes/indexers/events.py
@@ -9,5 +9,5 @@ def event_location(context, **kw):
     """
     luoghi_correlati = [x.to_object for x in context.luoghi_correlati]
     luoghi_correlati = filter(bool, luoghi_correlati)
-    luoghi_correlati_title = [x.title for x in luoghi_correlati]
+    luoghi_correlati_title = [x.UID() for x in luoghi_correlati]
     return luoghi_correlati_title
