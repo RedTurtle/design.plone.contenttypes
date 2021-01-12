2d6e808cf29c6fbe0ed969d7434a568a211cbf44
Andrea Cecchi <andrea.cecchi85@gmail.com>
2021-01-12 15:45:30 +0100

    add indexer for new field

--- src/design/plone/contenttypes/adapters/searchabletext_indexers.py
+++ src/design/plone/contenttypes/adapters/searchabletext_indexers.py
@@ -2,12 +2,17 @@
 from collective.dexteritytextindexer.converters import (
     DefaultDexterityTextIndexFieldConverter,
 )
-from collective.dexteritytextindexer.interfaces import IDexterityTextIndexFieldConverter
+from collective.dexteritytextindexer.interfaces import (
+    IDexterityTextIndexFieldConverter,
+)
+from design.plone.contenttypes.fields import IBlocksField
+from plone.dexterity.interfaces import IDexterityContent
+from plone.restapi.interfaces import IBlockSearchableText
+from z3c.form.interfaces import IWidget
 from z3c.relationfield.interfaces import IRelationChoice
 from z3c.relationfield.interfaces import IRelationList
-from plone.dexterity.interfaces import IDexterityContent
-from z3c.form.interfaces import IWidget
 from zope.component import adapter
+from zope.component import queryMultiAdapter
 from zope.interface import implementer
 
 
@@ -31,4 +36,34 @@ class RelationListFieldConverter(DefaultDexterityTextIndexFieldConverter):
         relations = self.field.get(self.context)
         if not relations:
             return ""
-        return " ".join([x.to_object.Title() for x in relations if x.to_object])
+        return " ".join(
+            [x.to_object.Title() for x in relations if x.to_object]
+        )
+
+
+@implementer(IDexterityTextIndexFieldConverter)
+@adapter(IDexterityContent, IBlocksField, IWidget)
+class BlocksFieldConverter(DefaultDexterityTextIndexFieldConverter):
+    def convert(self):
+        value = self.field.get(self.context)
+        blocks = value.get("blocks", {})
+        if not blocks:
+            return ""
+
+        blocks_text = []
+        for block in blocks.values():
+            searchableText = block.get("searchableText", "")
+            if searchableText:
+                blocks_text.append(searchableText)
+            block_type = block.get("@type", "")
+            adapter = queryMultiAdapter(
+                (self.context, self.context.REQUEST),
+                IBlockSearchableText,
+                name=block_type,
+            )
+
+            if adapter is not None:
+                text = adapter(block)
+                if text:
+                    blocks_text.append(text)
+        return " ".join(blocks_text)
