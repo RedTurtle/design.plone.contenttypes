3b1c78ffe4cbaee2d33ba6b938073e0a7aa36cc6
Daniele Andreotti <daniele.andreotti@redturtle.it>
2021-05-20 09:44:14 +0200

    Back to development: 3.0.4

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ long_description = "\n\n".join(
 
 setup(
     name="design.plone.contenttypes",
-    version="3.0.3",
+    version="3.0.4.dev0",
     description="DesignItalia contenty types",
     long_description=long_description,
     long_description_content_type="text/markdown",
