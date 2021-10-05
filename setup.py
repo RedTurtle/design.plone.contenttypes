4cc7e66befcea42ac6e63d10fdf29ac03c0523d9
Andrea Cecchi <andrea.cecchi85@gmail.com>
2021-10-05 10:10:16 +0200

    Back to development: 3.6.3

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ long_description = "\n\n".join(
 
 setup(
     name="design.plone.contenttypes",
-    version="3.6.2",
+    version="3.6.3.dev0",
     description="DesignItalia contenty types",
     long_description=long_description,
     long_description_content_type="text/markdown",
