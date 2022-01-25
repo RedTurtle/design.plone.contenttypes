4a9210bfc0408efd384654e3737e19f848781d2e
Andrea Cecchi <andrea.cecchi85@gmail.com>
2022-01-25 11:22:56 +0100

    fix event handler

--- src/design/plone/contenttypes/events/common.py
+++ src/design/plone/contenttypes/events/common.py
@@ -4,6 +4,6 @@
 def onModify(context, event):
 
     for description in event.descriptions:
-        if "IBasic.title" in description.attributes:
+        if "IBasic.title" in getattr(description, "attributes", []):
             for child in context.listFolderContents():
                 child.reindexObject(idxs=["parent"])
