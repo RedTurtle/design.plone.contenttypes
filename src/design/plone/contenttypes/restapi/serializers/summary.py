dfdb312003e9672609e4cdb9bdad53107fad6648
Martina Bustacchini <martina.bustacchini@redturtle.it>
2023-01-12 10:04:47 +0100

    fix pre merge conflicts

--- src/design/plone/contenttypes/restapi/serializers/summary.py
+++ src/design/plone/contenttypes/restapi/serializers/summary.py
@@ -4,6 +4,7 @@ from collective.taxonomy.interfaces import ITaxonomy
 from design.plone.contenttypes.interfaces import IDesignPloneContenttypesLayer
 from design.plone.contenttypes.interfaces.incarico import IIncarico
 from design.plone.contenttypes.interfaces.persona import IPersona
+from design.plone.contenttypes.interfaces.punto_di_contatto import IPuntoDiContatto
 from plone import api
 from plone.restapi.interfaces import ISerializeToJsonSummary
 from plone.restapi.serializer.converters import json_compatible
@@ -150,18 +151,28 @@ class IncaricoDefaultJSONSummarySerializer(DefaultJSONSummarySerializer):
 
 
 @implementer(ISerializeToJsonSummary)
+@adapter(IPuntoDiContatto, IDesignPloneContenttypesLayer)
+class PuntoDiContattoDefaultJSONSummarySerializer(DefaultJSONSummarySerializer):
+    def __call__(self, force_all_metadata=False):
+        res = super().__call__(force_all_metadata=force_all_metadata)
+        res["value_punto_contatto"] = self.context.value_punto_contatto
+        return res
+
+
+@implementer(ISerializeToJsonSummary)
 @adapter(IPersona, IDesignPloneContenttypesLayer)
 class PersonaDefaultJSONSummarySerializer(DefaultJSONSummarySerializer):
     def __call__(self, force_all_metadata=False):
         res = super().__call__(force_all_metadata=force_all_metadata)
         fields = dict(getFieldsInOrder(IPersona))
-        field = fields["foto_persona"]
-        images_info_adapter = getMultiAdapter(
-            (field, self.context, IDesignPloneContenttypesLayer)
-        )
-        if images_info_adapter:
-            res["image_scales"] = {
-                "foto_persona": [images_info_adapter()],
-            }
-        res["image_field"] = "foto_persona"
+        field = fields.get("foto_persona", None)
+        if field:
+            images_info_adapter = getMultiAdapter(
+                (field, self.context, IDesignPloneContenttypesLayer)
+            )
+            if images_info_adapter:
+                res["image_scales"] = {
+                    "foto_persona": [images_info_adapter()],
+                }
+            res["image_field"] = "foto_persona"
         return res
