0aadbc32dd55ab9aa59ac8e8f4e8cef7797d0f0a
Andrea Cecchi <andrea.cecchi85@gmail.com>
2023-03-16 17:27:02 +0100

    enable behavior in venue ct

--- src/design/plone/contenttypes/upgrades/upgrades.py
+++ src/design/plone/contenttypes/upgrades/upgrades.py
@@ -1003,3 +1003,25 @@ def to_5500(context):
                     if blocks:
                         fix_block(blocks, argomenti_mapping)
                         setattr(item, name, value)
+
+
+def to_5600(context):
+    portal_types = api.portal.get_tool(name="portal_types")
+    behaviors = list(portal_types["Venue"].behaviors)
+    if "plone.excludefromnavigation" in behaviors:
+        return
+    logger.info("Enable plone.excludefromnavigation behavior")
+    behaviors.append("plone.excludefromnavigation")
+    portal_types["Venue"].behaviors = tuple(behaviors)
+    logger.info("Reindex Venue objects")
+    brains = api.content.find(portal_type="Venue")
+    tot = len(brains)
+    i = 0
+    for brain in brains:
+        i += 1
+        if i % 100 == 0:
+            logger.info("Progress: {}/{}".format(i, tot))
+        venue = brain.getObject()
+        if not getattr(venue, "exclude_from_nav", None):
+            setattr(venue, "exclude_from_nav", False)
+            venue.reindexObject(idxs=["exclude_from_nav"])
