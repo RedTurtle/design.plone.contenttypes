66b823186fa5c1df5dba3e42d7e72e73835f6c43
Luca Bellenghi <lucabel@gmail.com>
2023-04-28 07:46:01 +0200

    Back to development: 6.0.6

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ long_description = "\n\n".join(
 
 setup(
     name="design.plone.contenttypes",
-    version="6.0.5",
+    version="6.0.6.dev0",
     description="DesignItalia contenty types",
     long_description=long_description,
     long_description_content_type="text/markdown",
