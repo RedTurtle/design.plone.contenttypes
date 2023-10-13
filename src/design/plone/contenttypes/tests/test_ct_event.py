cea20d688cb5877355acb6a499a873b08b0286c2
Andrea Cecchi <andrea.cecchi85@gmail.com>
2023-10-13 10:03:38 +0200

    improved tests

--- src/design/plone/contenttypes/tests/test_ct_event.py
+++ src/design/plone/contenttypes/tests/test_ct_event.py
@@ -4,9 +4,6 @@ from design.plone.contenttypes.schema_overrides import SchemaTweaks
 from design.plone.contenttypes.testing import (
     DESIGN_PLONE_CONTENTTYPES_API_FUNCTIONAL_TESTING,
 )
-from design.plone.contenttypes.testing import (
-    DESIGN_PLONE_CONTENTTYPES_INTEGRATION_TESTING,
-)
 from plone import api
 from plone.app.testing import setRoles
 from plone.app.testing import SITE_OWNER_NAME
@@ -20,13 +17,23 @@ import transaction
 import unittest
 
 
-class TestEvent(unittest.TestCase):
-    layer = DESIGN_PLONE_CONTENTTYPES_INTEGRATION_TESTING
+class TestEventSchema(unittest.TestCase):
+    layer = DESIGN_PLONE_CONTENTTYPES_API_FUNCTIONAL_TESTING
     maxDiff = None
 
     def setUp(self):
-        """Custom shared utility setup for tests."""
+        self.app = self.layer["app"]
         self.portal = self.layer["portal"]
+        self.request = self.layer["request"]
+        self.portal_url = self.portal.absolute_url()
+        setRoles(self.portal, TEST_USER_ID, ["Manager"])
+
+        self.api_session = RelativeSession(self.portal_url)
+        self.api_session.headers.update({"Accept": "application/json"})
+        self.api_session.auth = (SITE_OWNER_NAME, SITE_OWNER_PASSWORD)
+
+    def tearDown(self):
+        self.api_session.close()
 
     def test_behaviors_enabled_for_event(self):
         portal_types = api.portal.get_tool(name="portal_types")
@@ -72,6 +79,262 @@ class TestEvent(unittest.TestCase):
         setRoles(self.portal, TEST_USER_ID, ["Manager"])
         event = api.content.create(container=self.portal, type="Event", title="Evento")
         self.assertTrue(IDesignPloneContentType.providedBy(event))
+
+    def test_event_fieldsets(self):
+        """
+        Get the list from restapi
+        """
+        resp = self.api_session.get("@types/Event").json()
+        self.assertEqual(len(resp["fieldsets"]), 12)
+        # should be 13 but SchemaTweaks does not work in tests
+        # self.assertEqual(len(resp["fieldsets"]), 13)
+        self.assertEqual(
+            [x.get("id") for x in resp["fieldsets"]],
+            [
+                "default",
+                "cose",
+                "luogo",
+                "date_e_orari",
+                "costi",
+                "contatti",
+                "informazioni",
+                # "correlati", see SchemaTweaks problem in tests
+                "categorization",
+                "dates",
+                "settings",
+                "ownership",
+                "seo",
+            ],
+        )
+
+    def test_event_required_fields(self):
+        resp = self.api_session.get("@types/Event").json()
+        self.assertEqual(
+            sorted(resp["required"]),
+            sorted(
+                [
+                    "title",
+                    "tassonomia_argomenti",
+                    "tipologia_evento",
+                    "start",
+                    "end",
+                    "descrizione_estesa",
+                    "descrizione_destinatari",
+                    "contact_info",
+                    "description",
+                ]
+            ),
+        )
+
+    def test_event_fields_default_fieldset(self):
+        """
+        Get the list from restapi
+        """
+        resp = self.api_session.get("@types/Event").json()
+        self.assertEqual(
+            resp["fieldsets"][0]["fields"],
+            [
+                "title",
+                "description",
+                "start",
+                "end",
+                "whole_day",
+                "open_end",
+                "sync_uid",
+                "image",
+                "image_caption",
+                "preview_image",
+                "preview_caption",
+                "correlato_in_evidenza",
+                "tassonomia_argomenti",
+                "recurrence",
+                "sottotitolo",
+                "tipologia_evento",
+            ]
+            # should be like this with SchemaTweaks
+            # [
+            #     "title",
+            #     "description",
+            #     "image",
+            #     "image_caption",
+            #     "preview_image",
+            #     "preview_caption",
+            #     "correlato_in_evidenza",
+            #     "tassonomia_argomenti",
+            #     "sottotitolo",
+            #     "tipologia_evento",
+            # ],
+        )
+
+    def test_event_fields_cose_fieldset(self):
+        """
+        Get the list from restapi
+        """
+        resp = self.api_session.get("@types/Event").json()
+        self.assertEqual(
+            resp["fieldsets"][1]["fields"],
+            [
+                "descrizione_estesa",
+                "descrizione_destinatari",
+                "persone_amministrazione",
+            ],
+        )
+
+    def test_event_fields_luogo_fieldset(self):
+        """
+        Get the list from restapi
+        """
+        resp = self.api_session.get("@types/Event").json()
+        self.assertEqual(
+            resp["fieldsets"][2]["fields"],
+            [
+                "luoghi_correlati",
+                "nome_sede",
+                "street",
+                "zip_code",
+                "city",
+                "quartiere",
+                "circoscrizione",
+                "country",
+                "geolocation",
+            ],
+        )
+
+    def test_event_fields_date_e_orari_fieldset(self):
+        """
+        Get the list from restapi
+        """
+        resp = self.api_session.get("@types/Event").json()
+        self.assertEqual(
+            resp["fieldsets"][3]["fields"],
+            [
+                "orari",
+            ],
+            # with SchemaTweaks should be like this
+            # [
+            #     "start",
+            #     "end",
+            #     "whole_day",
+            #     "open_end",
+            #     "sync_uid",
+            #     "recurrence",
+            #     "orari",
+            # ],
+        )
+
+    def test_event_fields_costi_fieldset(self):
+        """
+        Get the list from restapi
+        """
+        resp = self.api_session.get("@types/Event").json()
+        self.assertEqual(
+            resp["fieldsets"][4]["fields"],
+            [
+                "prezzo",
+            ],
+            # should be like this with SchemaTweaks
+            # [
+            #     "costi",
+            # ],
+        )
+
+    def test_event_fields_contatti_fieldset(self):
+        """
+        Get the list from restapi
+        """
+        resp = self.api_session.get("@types/Event").json()
+        self.assertEqual(
+            resp["fieldsets"][5]["fields"],
+            [
+                "organizzato_da_interno",
+                "organizzato_da_esterno",
+                "supportato_da",
+                "patrocinato_da",
+                "contact_info",
+            ],
+        )
+
+    def test_event_fields_informazioni_fieldset(self):
+        """
+        Get the list from restapi
+        """
+        resp = self.api_session.get("@types/Event").json()
+        self.assertEqual(
+            resp["fieldsets"][6]["fields"],
+            ["ulteriori_informazioni", "strutture_politiche"],
+        )
+
+    # def test_event_fields_correlati_fieldset(self):
+    #     """
+    #     Get the list from restapi
+    #     """
+    #     resp = self.api_session.get("@types/Event").json()
+    #     self.assertEqual(
+    #         resp["fieldsets"][7]["fields"],
+    #         ["relatedItems"],
+    #     )
+
+    def test_event_fields_categorization_fieldset(self):
+        """
+        Get the list from restapi
+        """
+        resp = self.api_session.get("@types/Event").json()
+        self.assertEqual(
+            resp["fieldsets"][7]["fields"],
+            ["subjects", "language", "relatedItems"]
+            # should be like this with SchemaTweaks
+            # ["subjects", "language"],
+        )
+
+    def test_event_fields_dates_fieldset(self):
+        """
+        Get the list from restapi
+        """
+        resp = self.api_session.get("@types/Event").json()
+        self.assertEqual(resp["fieldsets"][8]["fields"], ["effective", "expires"])
+
+    def test_event_fields_settings_fieldset(self):
+        """
+        Get the list from restapi
+        """
+        resp = self.api_session.get("@types/Event").json()
+        self.assertEqual(
+            resp["fieldsets"][9]["fields"],
+            [
+                "allow_discussion",
+                "exclude_from_nav",
+                "id",
+                "versioning_enabled",
+                "changeNote",
+            ],
+        )
+
+    def test_event_fields_ownership_fieldset(self):
+        """
+        Get the list from restapi
+        """
+        resp = self.api_session.get("@types/Event").json()
+        self.assertEqual(
+            resp["fieldsets"][10]["fields"], ["creators", "contributors", "rights"]
+        )
+
+    def test_event_fields_seo_fieldset(self):
+        """
+        Get the list from restapi
+        """
+        resp = self.api_session.get("@types/Event").json()
+        self.assertEqual(
+            resp["fieldsets"][11]["fields"],
+            [
+                "seo_title",
+                "seo_description",
+                "seo_noindex",
+                "seo_canonical_url",
+                "opengraph_title",
+                "opengraph_description",
+                "opengraph_image",
+            ],
+        )
 
 
 class TestEventApi(unittest.TestCase):
--- src/design/plone/contenttypes/tests/test_ct_persona.py
+++ src/design/plone/contenttypes/tests/test_ct_persona.py
@@ -3,10 +3,8 @@
 from design.plone.contenttypes.testing import (
     DESIGN_PLONE_CONTENTTYPES_API_FUNCTIONAL_TESTING,
 )
-from design.plone.contenttypes.testing import (
-    DESIGN_PLONE_CONTENTTYPES_INTEGRATION_TESTING,
-)
 from plone import api
+from plone.app.testing import helpers
 from plone.app.testing import setRoles
 from plone.app.testing import SITE_OWNER_NAME
 from plone.app.testing import SITE_OWNER_PASSWORD
@@ -22,12 +20,29 @@ from zope.intid.interfaces import IIntIds
 import unittest
 
 
-class TestPersona(unittest.TestCase):
-    layer = DESIGN_PLONE_CONTENTTYPES_INTEGRATION_TESTING
+class TestPersonaSchema(unittest.TestCase):
+    layer = DESIGN_PLONE_CONTENTTYPES_API_FUNCTIONAL_TESTING
 
     def setUp(self):
-        """Custom shared utility setup for tests."""
+        self.app = self.layer["app"]
         self.portal = self.layer["portal"]
+        self.request = self.layer["request"]
+        self.portal_url = self.portal.absolute_url()
+        setRoles(self.portal, TEST_USER_ID, ["Manager"])
+
+        self.api_session = RelativeSession(self.portal_url)
+        self.api_session.headers.update({"Accept": "application/json"})
+        self.api_session.auth = (SITE_OWNER_NAME, SITE_OWNER_PASSWORD)
+        self.persona = api.content.create(
+            container=self.portal, type="Persona", title="John Doe"
+        )
+        intids = getUtility(IIntIds)
+
+        self.persona_ref = RelationValue(intids.getId(self.persona))
+        commit()
+
+    def tearDown(self):
+        self.api_session.close()
 
     def test_behaviors_enabled_for_persona(self):
         portal_types = api.portal.get_tool(name="portal_types")
@@ -57,33 +72,143 @@ class TestPersona(unittest.TestCase):
         portal_types = api.portal.get_tool(name="portal_types")
         self.assertEqual("Persona pubblica", portal_types["Persona"].title)
 
-
-class TestPersonaEndpoint(unittest.TestCase):
-    """"""
-
-    layer = DESIGN_PLONE_CONTENTTYPES_API_FUNCTIONAL_TESTING
-
-    def setUp(self):
-        self.app = self.layer["app"]
-        self.portal = self.layer["portal"]
-        self.request = self.layer["request"]
-        self.portal_url = self.portal.absolute_url()
-        setRoles(self.portal, TEST_USER_ID, ["Manager"])
-
-        self.api_session = RelativeSession(self.portal_url)
-        self.api_session.headers.update({"Accept": "application/json"})
-        self.api_session.auth = (SITE_OWNER_NAME, SITE_OWNER_PASSWORD)
-
-        self.persona = api.content.create(
-            container=self.portal, type="Persona", title="John Doe"
+    def test_persona_fieldsets(self):
+        """
+        Get the list from restapi
+        """
+        resp = self.api_session.get("@types/Persona").json()
+        self.assertEqual(len(resp["fieldsets"]), 10)
+        self.assertEqual(
+            [x.get("id") for x in resp["fieldsets"]],
+            [
+                "default",
+                "ruolo",
+                "contatti",
+                "documenti",
+                "informazioni",
+                # "correlati", questo non viene fuori nei test
+                "categorization",
+                "dates",
+                "ownership",
+                "settings",
+                "seo",
+            ],
         )
-        intids = getUtility(IIntIds)
 
-        self.persona_ref = RelationValue(intids.getId(self.persona))
-        commit()
+    def test_persona_required_fields(self):
+        resp = self.api_session.get("@types/Persona").json()
+        self.assertEqual(
+            sorted(resp["required"]),
+            sorted(
+                [
+                    "title",
+                    "contact_info",
+                ]
+            ),
+        )
 
-    def tearDown(self):
-        self.api_session.close()
+    def test_persona_fields_default_fieldset(self):
+        """
+        Get the list from restapi
+        """
+        resp = self.api_session.get("@types/Persona").json()
+        self.assertEqual(
+            resp["fieldsets"][0]["fields"],
+            ["title", "description", "foto_persona"],
+        )
+
+    def test_persona_fields_ruolo_fieldset(self):
+        """
+        Get the list from restapi
+        """
+        resp = self.api_session.get("@types/Persona").json()
+        self.assertEqual(
+            resp["fieldsets"][1]["fields"],
+            ["incarichi_persona", "competenze", "deleghe", "biografia"],
+        )
+
+    def test_persona_fields_contatti_fieldset(self):
+        """
+        Get the list from restapi
+        """
+        resp = self.api_session.get("@types/Persona").json()
+        self.assertEqual(resp["fieldsets"][2]["fields"], ["contact_info"])
+
+    def test_persona_fields_documenti_fieldset(self):
+        """
+        Get the list from restapi
+        """
+        resp = self.api_session.get("@types/Persona").json()
+        self.assertEqual(resp["fieldsets"][3]["fields"], ["curriculum_vitae"])
+
+    def test_persona_fields_informazioni_fieldset(self):
+        """
+        Get the list from restapi
+        """
+        resp = self.api_session.get("@types/Persona").json()
+        self.assertEqual(
+            resp["fieldsets"][4]["fields"],
+            ["ulteriori_informazioni"],
+        )
+
+    def test_persona_fields_categorization_fieldset(self):
+        """
+        Get the list from restapi
+        """
+        resp = self.api_session.get("@types/Persona").json()
+        self.assertEqual(
+            resp["fieldsets"][5]["fields"], ["relatedItems", "subjects", "language"]
+        )
+
+    def test_persona_fields_dates_fieldset(self):
+        """
+        Get the list from restapi
+        """
+        resp = self.api_session.get("@types/Persona").json()
+        self.assertEqual(resp["fieldsets"][6]["fields"], ["effective", "expires"])
+
+    def test_persona_fields_ownership_fieldset(self):
+        """
+        Get the list from restapi
+        """
+        resp = self.api_session.get("@types/Persona").json()
+        self.assertEqual(
+            resp["fieldsets"][7]["fields"], ["creators", "contributors", "rights"]
+        )
+
+    def test_persona_fields_settings_fieldset(self):
+        """
+        Get the list from restapi
+        """
+        resp = self.api_session.get("@types/Persona").json()
+        self.assertEqual(
+            resp["fieldsets"][8]["fields"],
+            [
+                "allow_discussion",
+                "exclude_from_nav",
+                "id",
+                "versioning_enabled",
+                "changeNote",
+            ],
+        )
+
+    def test_persona_fields_seo_fieldset(self):
+        """
+        Get the list from restapi
+        """
+        resp = self.api_session.get("@types/Persona").json()
+        self.assertEqual(
+            resp["fieldsets"][9]["fields"],
+            [
+                "seo_title",
+                "seo_description",
+                "seo_noindex",
+                "seo_canonical_url",
+                "opengraph_title",
+                "opengraph_description",
+                "opengraph_image",
+            ],
+        )
 
     def test_atto_di_nomina_incarico(self):
         incarico = api.content.create(
@@ -130,3 +255,28 @@ class TestPersonaEndpoint(unittest.TestCase):
         )()
         # non ho incarichi, ma soprattutto non ho errori
         self.assertTrue(len(summary["incarichi"]) == 0)
+
+    @unittest.skip
+    def test_unauthorized_on_subfolder(self):
+        incarico = api.content.create(
+            container=self.persona.incarichi, type="Incarico", title="Sindaco"
+        )
+        commit()
+        intids = getUtility(IIntIds)
+        self.persona.incarichi_persona = [RelationValue(intids.getId(incarico))]
+        api.content.transition(obj=self.persona, transition="publish")
+        commit()
+
+        helpers.logout()
+        # with previous bug this as anonymous user return
+        # AccessControl.unauthorized.Unauthorized: You are not allowed to
+        # access '_Access_inactive_portal_content_Permission' in this context
+        persona_summary = getMultiAdapter(
+            (self.persona, self.request), ISerializeToJsonSummary
+        )()
+        self.assertEqual(persona_summary["incarichi"], "Sindaco")
+        incarico_summary = getMultiAdapter(
+            (self.persona.incarichi.sindaco, self.request), ISerializeToJsonSummary
+        )()
+        self.assertEqual(incarico_summary["compensi_file"], [])
+        self.assertEqual(incarico_summary["importi_di_viaggio_e_o_servizi"], [])
